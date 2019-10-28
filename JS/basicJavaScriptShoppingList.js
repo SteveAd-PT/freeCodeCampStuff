7ac8a02b735d9637cd60a303672f29aaaf0d058c
Steven Adler <50134608+SteveAd-PT@users.noreply.github.com>
2019-10-28 15:29:07 +0100

    Add files via upload

---
+++ JS/basicJavaScriptShoppingList.js
@@ -0,0 +1,9 @@
+/* Creating/manipulating arrays using bracket notation 
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/shopping-list */
+
+var myList = [];
+myList[0] = ["Bananas", 5];
+myList[1] = ["Bread", 1];
+myList[2] = ["Tomatoes", 6];
+myList[3] = ["Carrots", 4];
+myList[4] = ["Cucumber", 2];
